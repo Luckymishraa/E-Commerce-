dbe07130064c760f98269d9759293103e71c24d7
Luckymishraa <86152604+Luckymishraa@users.noreply.github.com>
2023-11-16 00:18:50 +0530

    update

--- script.js
+++ script.js
@@ -13,3 +13,9 @@ if(close){
         nav.classList.remove('active');
     })
 }
+
+// JavaScript function to toggle the navigation menu
+function openNavbar() {
+	const navMenu = document.getElementById("nav-menu");
+	navMenu.classList.toggle("active");
+}
